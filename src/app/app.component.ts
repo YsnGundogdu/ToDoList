28b134a13e3179bab1400086944bbd841fb03405
YsnGundogdu <ysn_gundogdu@hotmail.com>
2023-12-21 21:23:34 +0300

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-angular';
+export class AppComponent implements OnInit {
+
+  list : ListModel [] = [];
+  workInput : any;
+  isChecked : boolean = false;
+
+  ngOnInit(): void {
+  }
+
+  constructor(){}
+
+  add(){
+    if (this.workInput != null) {
+      this.list.push({
+        id : this.list.length,
+        isComplete : false,
+        work : this.workInput
+      });
+  
+      this.workInput = null;  
+    }
+  }
+
+  remove(index:any){
+    this.list.splice(index, 1);
+  }
+}
+
+
+
+
+export interface ListModel{
+  id:number;
+  work:string;
+  isComplete:boolean;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
